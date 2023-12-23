c89b8dcf6c04f840acad2e76530673c74c814307
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-12-23 18:49:50 -0500

    Finished script, need to test.

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-function playRound(playerSelection, computerSelection) {
-  // Where program will be written:
-}
- 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function getComputerChoice() {
+  const choices = ['Rock', 'Paper', 'Scissors']
 
-function game()
+  return choices [getRandomChoice()]; 
+}
+
+function getRandomChoice() {
+  
+  return (getRandomChoice.counter++ % 3);
+}
+
+getRandomChoice.counter=0;
+
+function playRound (playerSelection, computerSelection) {
+  
+  const playerChoice = playerSelection.toLowerCase();
+  const computerChoice = getComputerChoice().toLowerCase();
+
+ 
+  if (playerChoice === computerChoice) {
+    return "Tie";
+  } else {
+    const winConditions = {
+      rock: 'scissors',
+      paper: 'rock',
+      scissors: 'paper'
+    };
+
+if (winConditions[playerChoice] === computerChoice) {
+
+  return "Win";
+} else {
+  return "Lose";
+    }
+  }
+}
+
+
+function game(rounds) {
+
+  let currentRound = 1;
+
+  while (currentRound <= rounds) {
+    const playerSelection = prompt("Enter: rock, paper, scissors")
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    alert(result);
+
+    currentRound++;
+  }
+
+  alert("Game ended");
+
+}
+game (5);
