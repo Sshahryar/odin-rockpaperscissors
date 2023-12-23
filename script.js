5dfa40b4934b1511726e2c0b2d5ef4c82fe43e27
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-12-23 17:23:53 -0500

    testing

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-function playRound(playerSelection, computerSelection) {
-  // Where program will be written:
-}
- 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log("hello world");
 
