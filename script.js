c858ca16c2bfc259db2f8b0f90cbb09d2cf3e24a
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-12-23 16:35:02 -0500

    Added function given with project instructions

---
+++ script.js
@@ -0,0 +1,7 @@
+function playRound(playerSelection, computerSelection) {
+  // Where program will be written:
+}
+ 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
