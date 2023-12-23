e107d2da68d120893339df44ef5d7347ada81f46
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-12-23 17:36:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("hello world");
+function playRound(playerSelection, computerSelection) {
+  // Where program will be written:
+}
+ 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
+function game()
